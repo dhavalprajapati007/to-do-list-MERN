17884aed672999c999a70214d8284d287dc862ec
Dhaval Prajapati <dhavalprajapati01052@gmail.com>
2024-09-22 03:27:40 +0530

    To-do List Project Initial commit

---
+++ server/src/models/todo.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const todoSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    title: { type: String, required: true },
+    completed: { type: Boolean, default: false },
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+export default Todo;
